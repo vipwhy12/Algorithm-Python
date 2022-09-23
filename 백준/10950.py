eb2827bdd2d1d31e29ccc2dce67e9eec27ba8db0
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-09-23 15:02:54 +0900

    BAEKJOON_10950

---
+++ 백준/10950.py
@@ -0,0 +1,10 @@
+#문제: 두 정수와 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+#입력: 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에는 A와 B가 주어진다.
+#출력: 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다.(0 < A, B < 10 )
+import sys
+
+repeat = int(sys.stdin.readline())
+
+for i in range(repeat):
+    a,b = map(int, sys.stdin.readline().split(" "))
+    print(a + b)
