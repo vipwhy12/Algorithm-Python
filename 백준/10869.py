2fe3ca35a5cb674fdcd1eda039708601adbce202
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-09-23 11:10:00 +0900

    BAEKJOON_10869

--- 백준/10869.py
+++ 백준/10869.py
@@ -1,2 +1,12 @@
-A + B
-A - B = C
+#문제 : 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+#입력 : 두 자연수 A와 B 가 주어진다.  1<= A, B <= 10000
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+
+if 1 <= a and b <= 1000 :
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a//b)
+    print(a%b)
