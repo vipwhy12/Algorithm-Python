fc347e16f17f0ed6ba1937f3ba3f79c49dfbf129
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-09-23 16:43:09 +0900

    BAEKJOON_10871

--- 백준/2562.py
+++ 백준/2562.py
@@ -1,6 +1,6 @@
 #문제 : 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오
-import sys
 
+import sys
 list = []
 
 #입력된 값을 list에 넣기
@@ -12,4 +12,3 @@ max_index = list.index(list_max)
 
 print(list_max)
 print(max_index +1)
-
