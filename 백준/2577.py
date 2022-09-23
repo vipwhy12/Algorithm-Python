9ee1b8c955779cc69243b8501e4bc99e5d1ace01
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-09-23 23:10:26 +0900

    BAEKJOON

---
+++ 백준/2577.py
@@ -0,0 +1,15 @@
+# 숫자의 개수
+# 문제 : 세 개의 자연수 A, B, C가 주어질 때 A × B × C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오.
+import sys
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+multiplication = A * B * C
+
+for i in range(len(multiplication)):
+    
+    for j in range(9):
+        if : 
+    
