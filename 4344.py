5e3b66cc982336487331cde85e041c56707fbb52
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-09-23 22:24:21 +0900

    BAEKJOON_4344

--- 4344.py
+++ 4344.py
@@ -3,13 +3,26 @@
 # 입력 : 첫째 줄에는 테스트 케이스 개수 C가 주어진다. 둘째 줄부터 각 케이스마다 학생의 수 N(1 <= N <= 1000, N은 정수)
 import sys
 
+#반복할 횟수 입력
 repeat = int(sys.stdin.readline())
 
 for i in range(repeat) :
-    score = list(map(int(), sys.stdin.readline().split(" ")))
-    average = 0
-    for j in range(1, score[0]):
-        average += j
-    #for j in range(int(sys.stdin.readline())):
-    #score = list(int(sys.stdin.readline()) for j in range(int(sys.stdin.readline())))
-    print(average /score[0])
+    #점수 리스트 생성
+    count = 0
+    temp = 0
+    score = list(map(int, input().split(" ")))
+
+    #학생들의 평균을 구하기
+    for j in range(1, score[0] + 1):
+        temp += score[j]
+    
+        
+    average = temp // score[0]
+    
+    
+    # 만약 평균 보다 크다면 
+    for j in range(1, score[0] + 1):
+        if average < score[j]:
+            count += 1
+            
+    print(("{:.3f}".format(count / score[0] * 100)) + "%")
