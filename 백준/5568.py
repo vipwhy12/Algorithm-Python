417655d383c4d2b44a93ca502d5cbd3cbb693dba
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-09-27 13:38:21 +0900

    카드놓기

--- 백준/5568.py
+++ 백준/5568.py
@@ -1,42 +1,34 @@
-# 카드 놓기 
-# 문제 : 상근이는 카드 n(4 ≤ n ≤ 10)장을 바닥에 나란히 놓고 놀고있다. 각 카드에는 1이상 99이하의 정수가 적혀져 있다. 상근이는 이 카드 중에서 k(2 ≤ k ≤ 4)장을 선택하고, 가로로 나란히 정수를 만들기로 했다. 상근이가 만들 수 있는 정수는 모두 몇 가지일까?
-#       예를 들어, 카드가 5장 있고, 카드에 쓰여 있는 수가 1, 2, 3, 13, 21라고 하자. 여기서 3장을 선택해서 정수를 만들려고 한다. 2, 1, 13을 순서대로 나열하면 정수 2113을 만들 수 있다. 또, 21, 1, 3을 순서대로 나열하면 2113을 만들 수 있다. 이렇게 한 정수를 만드는 조합이 여러 가지 일 수 있다.
-#       n장의 카드에 적힌 숫자가 주어졌을 때, 그 중에서 k개를 선택해서 만들 수 있는 정수의 개수를 구하는 프로그램을 작성하시오.
 import sys
-#sys.stdin = open("input.txt","r")
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-k = int(sys.stdin.readline())
+n = int(input())
+k = int(input())
 
-list_N = []
-for i in range(N):
-    list_N.append(int(sys.stdin.readline()))
+cards = []
+visited = [False for _ in range(n)]
 
-print(list_N)
+for _ in range(n):
+    cards.append(input().rstrip())
 
-visited = [False for _ in range (k)]
-print(visited)
-
-
-result = []
+# 만든 수열을 저장할 집합
+result_set = set()
 
 def solution(depth, string):
-    #상근이가 카드를 다 뽑았을 때
+    # 가장 끝(바닥)까지 내려갔을 경우
     if depth == k:
-        print(string)
-        result.append(string)
+        # 집합에 추가하기(문자열로 하는 이유: set의 원소로는 리스트가 들어갈 수 없음)
+        result_set.add(string)
+        # 끝까지 내려갔으니까 이 수열은 끝났음
         return
-    for i in range(len(visited)):
-        #내가 뽑았으면 False를 넘겨주자 
+    for i in range(len(cards)):
+        # 아직 사용하지 않은 카드인지
         if visited[i] == False:
-            # 방문했습니다.
-            visited[i] == True
-            # 더 깊이 들어갈께요
-            #k = k - 1
-            solution(depth + 1, string + str(list_N[i]))
-            #다시 올라가는거에요
+            # 이번에 사용할거고
+            visited[i] = True
+            # 한층 더 깊이 내려갔다 오겠음
+            solution(depth+1,string+cards[i])
+            # 마지막으로 사용하기로 한거 무효화
             visited[i] = False
-            
-            
+
 solution(0, '')
-print(len(result))
+print(len(result_set))
